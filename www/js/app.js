6c2b5fbe2d636ead5184b01657164d52230eb27e
Greg Hopkins <greg.hopkins@gmail.com>
2015-02-21 10:53:30 -0500

    applying 2 space tabs

--- www/js/app.js
+++ www/js/app.js
@@ -1,54 +1,53 @@
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    angular
-        .module('app', ['ionic'])
-        .run(function($ionicPlatform) {
-            $ionicPlatform.ready(function() {
-                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                // for form inputs)
-                if(window.cordova && window.cordova.plugins.Keyboard) {
-                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                }
+  angular
+    .module('app', ['ionic'])
+    .run(function ($ionicPlatform) {
+      $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
 
-                if(window.StatusBar) {
-                    StatusBar.styleDefault();
-                }
+        if (window.StatusBar) {
+          StatusBar.styleDefault();
+        }
 
-                if(window.plugins && window.plugins.pushNotification)
-                {
-                    var pushNotification = window.plugins.pushNotification;
+        if (window.plugins && window.plugins.pushNotification) {
+          var pushNotification = window.plugins.pushNotification;
 
-                    //set push notification callback before we initialize the plugin
-                    document.addEventListener('push-notification', function(event) {
-                        //get the notification payload
-                        var notification = event.notification;
+          //set push notification callback before we initialize the plugin
+          document.addEventListener('push-notification', function (event) {
+            //get the notification payload
+            var notification = event.notification;
 
-                        //display alert to the user for example
-                        alert(notification.aps.alert);
+            //display alert to the user for example
+            alert(notification.aps.alert);
 
-                        //clear the app badge
-                        pushNotification.setApplicationIconBadgeNumber(0);
-                    });
+            //clear the app badge
+            pushNotification.setApplicationIconBadgeNumber(0);
+          });
 
-                    //initialize the plugin
-                    pushNotification.onDeviceReady({pw_appid:"787CF-7C7BD"});
+          //initialize the plugin
+          pushNotification.onDeviceReady({pw_appid: "787CF-7C7BD"});
 
-                    //register for pushes
-                    pushNotification.registerDevice(
-                        function(status) {
-                            var deviceToken = status['deviceToken'];
-                            console.warn('registerDevice: ' + deviceToken);
-                        },
-                        function(status) {
-                            console.warn('failed to register : ' + JSON.stringify(status));
-                            alert(JSON.stringify(['failed to register ', status]));
-                        }
-                    );
+          //register for pushes
+          pushNotification.registerDevice(
+            function (status) {
+              var deviceToken = status['deviceToken'];
+              console.warn('registerDevice: ' + deviceToken);
+            },
+            function (status) {
+              console.warn('failed to register : ' + JSON.stringify(status));
+              alert(JSON.stringify(['failed to register ', status]));
+            }
+          );
 
-                    //reset badges on app start
-                    pushNotification.setApplicationIconBadgeNumber(0);
-                }
-            });
-        });
+          //reset badges on app start
+          pushNotification.setApplicationIconBadgeNumber(0);
+        }
+      });
+    });
 })();
--- www/js/app.routes.js
+++ www/js/app.routes.js
@@ -1,28 +1,28 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('app')
-        .config(function($stateProvider, $urlRouterProvider) {
+  angular
+    .module('app')
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-            //$stateProvider
-            //    .state('app', {
-            //        url: "/app",
-            //        abstract: true,
-            //        templateUrl: "templates/menu.html",
-            //        controller: 'AppCtrl'
-            //    })
-            //    .state('app.search', {
-            //        url: "/search",
-            //        views: {
-            //            'menuContent': {
-            //                templateUrl: "templates/search.html"
-            //            }
-            //        }
-            //    });
-            //
-            //// if none of the above states are matched, use this as the fallback
-            //$urlRouterProvider.otherwise('/app/playlists');
+      //$stateProvider
+      //    .state('app', {
+      //        url: "/app",
+      //        abstract: true,
+      //        templateUrl: "templates/menu.html",
+      //        controller: 'AppCtrl'
+      //    })
+      //    .state('app.search', {
+      //        url: "/search",
+      //        views: {
+      //            'menuContent': {
+      //                templateUrl: "templates/search.html"
+      //            }
+      //        }
+      //    });
+      //
+      //// if none of the above states are matched, use this as the fallback
+      //$urlRouterProvider.otherwise('/app/playlists');
 
-        });
+    });
 })();
