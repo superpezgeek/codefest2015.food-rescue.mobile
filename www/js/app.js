1ad387a3266925b8da0972eb2f14cd896b8e9505
raleighj <rrjohnson85@gmail.com>
2015-02-21 11:14:24 -0500

    Push

--- www/js/app.js
+++ www/js/app.js
@@ -1,53 +1,98 @@
-(function () {
+(function() {
   'use strict';
 
   angular
     .module('app', ['ionic'])
-    .run(function ($ionicPlatform) {
-      $ionicPlatform.ready(function () {
+    .run(function($ionicPlatform) {
+      $ionicPlatform.ready(function() {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
-        if (window.cordova && window.cordova.plugins.Keyboard) {
+        if(window.cordova && window.cordova.plugins.Keyboard) {
           cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
 
-        if (window.StatusBar) {
+        if(window.StatusBar) {
           StatusBar.styleDefault();
         }
 
-        if (window.plugins && window.plugins.pushNotification) {
-          var pushNotification = window.plugins.pushNotification;
-
-          //set push notification callback before we initialize the plugin
-          document.addEventListener('push-notification', function (event) {
-            //get the notification payload
-            var notification = event.notification;
-
-            //display alert to the user for example
-            alert(notification.aps.alert);
-
-            //clear the app badge
-            pushNotification.setApplicationIconBadgeNumber(0);
-          });
-
-          //initialize the plugin
-          pushNotification.onDeviceReady({pw_appid: "787CF-7C7BD"});
-
-          //register for pushes
-          pushNotification.registerDevice(
-            function (status) {
-              var deviceToken = status['deviceToken'];
-              console.warn('registerDevice: ' + deviceToken);
-            },
-            function (status) {
-              console.warn('failed to register : ' + JSON.stringify(status));
-              alert(JSON.stringify(['failed to register ', status]));
-            }
-          );
-
-          //reset badges on app start
-          pushNotification.setApplicationIconBadgeNumber(0);
+        if(window.plugins && window.plugins.pushNotification)
+        {
+          if($ionicPlatform.isIOS()) {
+            iosPushNotifications();
+          }
+          else if($ionicPlatform.isAndroid()) {
+            androidPushNotifictions();
+          }
+          else {
+            console.error("PUSH NOTIFICATIONS NOT WORKING");
+          }
         }
       });
     });
-})();
+
+
+  function iosPushNotifications() {
+    var pushNotification = window.plugins.pushNotification;
+
+    //set push notification callback before we initialize the plugin
+    document.addEventListener('push-notification', function(event) {
+      //get the notification payload
+      var notification = event.notification;
+
+      //display alert to the user for example
+      alert(notification.aps.alert);
+
+      //clear the app badge
+      pushNotification.setApplicationIconBadgeNumber(0);
+    });
+
+    //initialize the plugin
+    pushNotification.onDeviceReady({pw_appid:"787CF-7C7BD"});
+
+    //register for pushes
+    pushNotification.registerDevice(
+      function(status) {
+        var deviceToken = status['deviceToken'];
+        console.warn('registerDevice: ' + deviceToken);
+      },
+      function(status) {
+        console.warn('failed to register : ' + JSON.stringify(status));
+        alert(JSON.stringify(['failed to register ', status]));
+      }
+    );
+
+    //reset badges on app start
+    pushNotification.setApplicationIconBadgeNumber(0);
+  }
+
+  function androidPushNotifictions() {
+    var pushNotification = window.plugins.pushNotification;
+
+    //set push notifications handler
+    document.addEventListener('push-notification', function(event) {
+      var title = event.notification.title;
+      var userData = event.notification.userdata;
+
+      if(typeof(userData) != "undefined") {
+        console.warn('user data: ' + JSON.stringify(userData));
+      }
+
+      alert(title);
+    });
+
+    //initialize Pushwoosh with projectid: "GOOGLE_PROJECT_NUMBER", pw_appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
+    pushNotification.onDeviceReady({ projectid: "1040147572965", pw_appid : "787CF-7C7BD" });
+
+    //register for pushes
+    pushNotification.registerDevice(
+      function(status) {
+        var pushToken = status;
+        console.warn('push token: ' + pushToken);
+      },
+      function(status) {
+        console.warn(JSON.stringify(['failed to register ', status]));
+      }
+    );
+  }
+}
+)();
