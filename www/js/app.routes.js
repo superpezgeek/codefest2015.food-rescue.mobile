992803ea8d81cf68ecbd0067e4549617ce347141
Tony Perriello <tony.perriello@gmail.com>
2015-02-21 15:53:46 -0500

    start driver registration

--- www/js/app.routes.js
+++ www/js/app.routes.js
@@ -22,6 +22,31 @@
               templateUrl: 'views/overview.html'
             }
           }
+        })
+        .state('app.driverReg', {
+          url: '/driver-reg',
+          abstract: true,
+          views: {
+            'menuContent': {
+                template: '<ion-nav-view name="driverReg"></ion-nav-view>'
+            }
+          }
+        })
+        .state('app.driverReg.step1', {
+          url: '/step-1',
+          views: {
+            'driverReg': {
+              templateUrl: 'views/driverReg1.html'
+            }
+          }
+        })        
+        .state('app.driverReg.step2', {
+          url: '/step-2',
+          views: {
+            'driverReg': {
+              templateUrl: 'views/driverReg2.html'
+            }
+          }
         });
 
       // if none of the above states are matched, use this as the fallback
