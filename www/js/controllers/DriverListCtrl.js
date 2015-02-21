32cce00fbde84a7759fca84473898f097f8e7232
raleighj <rrjohnson85@gmail.com>
2015-02-21 18:22:28 -0500

    more driver and cowbell

--- www/js/controllers/DriverListCtrl.js
+++ www/js/controllers/DriverListCtrl.js
@@ -2,4 +2,15 @@ angular
   .module('app')
   .controller('DriverListCtrl', function($scope, $http, $cordovaGeolocation) {
     var posOptions = {timeout: 10000, enableHighAccuracy: false};
+    var position = {};
+
+    $cordovaGeolocation
+      .getCurrentPosition(posOptions)
+      .then(function (result) {
+        position.latitude  = result.coords.latitude;
+        position.longitude = result.coords.longitude;
+        console.log(position);
+      }, function(err) {
+        console.log(err);
+      });
   });
