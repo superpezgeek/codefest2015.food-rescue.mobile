4eae083623a1afd0db81df8e3a1efe7ec17a0016
Kyle Szives <kjszives@gmail.com>
2015-02-21 16:13:46 -0500

    added ins step 2

--- www/js/app.routes.js
+++ www/js/app.routes.js
@@ -13,11 +13,6 @@
           url: "/about",
           templateUrl: "views/about.html"
         })
-        .state('registration', {
-          url: "/registration",
-          abstract: true,
-          templateUrl: "views/registration.html"
-        })
         .state('app', {
           url: '/app',
           abstract: true,
@@ -31,9 +26,44 @@
               templateUrl: 'views/overview.html'
             }
           }
+        })
+        .state('app.registration', {
+          url: "/registration",
+          abstract: true,
+          views: {
+            'menuContent': {
+              template: '<ion-nav-view name="registrationContent"></ion-nav-view>'
+            }
+          }
+        })
+        .state('app.registration.donor', {
+          url: "/donor",
+          abstract: true,
+          views: {
+            'registrationContent': {
+              template: '<ion-nav-view name="registrationDonorContent"></ion-nav-view>'
+            }
+          }
+        })
+        .state('app.registration.donor.step1', {
+          url: "/step-1",
+          views: {
+            'registrationDonorContent': {
+              templateUrl: "views/registration/donor/step-1.html"
+            }
+          }
+        })
+        .state('app.registration.donor.step2', {
+          url: "/step-2",
+          views: {
+            'registrationDonorContent': {
+              templateUrl: "views/registration/donor/step-2.html"
+            }
+          }
         });
 
       // if none of the above states are matched, use this as the fallback
       $urlRouterProvider.otherwise('/login');
     });
+
 })();
