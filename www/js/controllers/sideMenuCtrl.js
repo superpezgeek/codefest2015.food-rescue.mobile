81ef1cc500585070fecba3e945311338024c49c4
raleighj <rrjohnson85@gmail.com>
2015-02-22 04:53:08 -0500

    side menu fix

--- www/js/controllers/sideMenuCtrl.js
+++ www/js/controllers/sideMenuCtrl.js
@@ -4,11 +4,13 @@ angular
     $scope.user = userService.user;
 
     $scope.hasDelivery = false;
-    driverService
-      .hasDelivery($scope.user.id)
-      .then(function(result) {
-        $scope.hasDelivery = result.data.length > 0;
-      });
+    if($scope.user !== null && $scope.user !== undefined) {
+      driverService
+        .hasDelivery($scope.user.id)
+        .then(function(result) {
+          $scope.hasDelivery = result.data.length > 0;
+        });
+    }
 
     $scope.logout = function() {
       $ionicHistory.clearHistory();
