b063d08b42b84db881382cc753054d80139a9907
raleighj <rrjohnson85@gmail.com>
2015-02-21 22:42:41 -0500

    sidemenu

--- www/js/controllers/LoginCtrl.js
+++ www/js/controllers/LoginCtrl.js
@@ -15,7 +15,10 @@ angular.module('app')
         function (value) {
           $ionicLoading.hide();
           $scope.showLoginError = false;
+
+          value.data.user.user_type = value.data.user_type;
           userService.user = value.data.user;
+
           $scope.user = {};
           $state.go('app.profile.main');
         },
